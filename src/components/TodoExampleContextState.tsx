028844ccdd1bb778da5bffc009e00977146fd254
Kasper Sandin <kasper.sandin@gmail.com>
2020-10-25 22:41:53 +0100

    Implement context example

--- src/components/TodoExampleContextState.tsx
+++ src/components/TodoExampleContextState.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import { Example } from "./Example";
-import { Typography } from "@material-ui/core";
+import { useStore } from "../lib/store/useStore";
+import { TodoExample } from "./TodoExample";
+import { Todo } from "../state/Todo";
+import { TodoId } from "../state/TodoId";
+import { Store } from "../lib/store/Store";
+import { createStore } from "../lib/store/createStore";
 
 export type TodoExampleContextStateProps = {};
 
-export const TodoExampleContextState: React.FC<TodoExampleContextStateProps> = () => (
-  <Example label="Todo app with context state">
-    <Typography>Not implemented</Typography>
-  </Example>
+let idCounter = 0;
+const nextId = () => idCounter++;
+const TodoStoreContext = createContext<Store<TodoId, Todo>>(
+  createStore(
+    (todo: Todo) => todo.id,
+    (todo: Todo) => ({ ...todo, id: nextId() as TodoId })
+  )
 );
+
+export const TodoExampleContextState: React.FC<TodoExampleContextStateProps> = () => {
+  const store = useContext(TodoStoreContext);
+  const [entries, addItem, updateItem, deleteItem] = useStore(store);
+  return (
+    <Example label="Todo app with context state">
+      <TodoExample
+        todos={entries.toList().toArray()}
+        createTodo={addItem}
+        updateTodo={updateItem}
+        deleteTodo={deleteItem}
+      />
+    </Example>
+  );
+};
