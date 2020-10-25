e164ab2d6a538c9fa3892f530ed0bc768fbfb03b
Kasper Sandin <kasper.sandin@gmail.com>
2020-10-25 18:35:17 +0100

    Add example for context state

--- src/components/Examples.tsx
+++ src/components/Examples.tsx
@@ -3,12 +3,14 @@ import { TodoExampleSharedState } from "./TodoExampleSharedState";
 import { TodoExampleIsolatedState } from "./TodoExampleIsolatedState";
 import { TodoExampleAsyncState } from "./TodoExampleAsyncState";
 import { TodoExamplePersistedAsyncState } from "./TodoExamplePersistedAsyncState";
+import { TodoExampleContextState } from "./TodoExampleContextState";
 
 export const Examples = () => {
   return (
     <>
       <TodoExampleAsyncState />
       <TodoExamplePersistedAsyncState />
+      <TodoExampleContextState />
       <TodoExampleSharedState />
       <TodoExampleIsolatedState />
     </>
---
+++ src/components/TodoExampleContextState.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Example } from "./Example";
+import { Typography } from "@material-ui/core";
+
+export type TodoExampleContextStateProps = {};
+
+export const TodoExampleContextState: React.FC<TodoExampleContextStateProps> = () => (
+  <Example label="Todo app with context state">
+    <Typography>Not implemented</Typography>
+  </Example>
+);
