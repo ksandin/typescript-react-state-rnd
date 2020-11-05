2e93b5249bb3bae4473e6afb730e61d49cf993ec
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-05 22:20:54 +0100

    Create express server for persisted remote state example

---
+++ src/server/todoExamplePersistedRemoteStateServer.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { TodoId } from "../shared/state/TodoId";
+import { Todo } from "../shared/state/Todo";
+import { createCrudMemoryAdapter } from "../lib/crud/createCrudMemoryAdapter";
+import { createNumericCrudIdentityFactory } from "../lib/crud/createNumericCrudIdentityFactory";
+
+const app = express();
+app.use(cors());
+const jsonParser = bodyParser.json();
+
+const adapter = createCrudMemoryAdapter<TodoId, Todo>(
+  createNumericCrudIdentityFactory(
+    (todo) => todo.id,
+    (todo, id) => ({ ...todo, id })
+  )
+);
+
+// Create
+app.post("/todo", jsonParser, async (req, res) =>
+  res.json(await adapter.create(req.body as Todo))
+);
+
+// readAll
+app.get("/todo", async (req, res) => res.json(await adapter.readAll()));
+
+// Update
+app.put("/todo", jsonParser, async (req, res) =>
+  res.json(await adapter.update(req.body as Todo))
+);
+
+// Delete
+app.delete("/todo/:id", async (req, res) => {
+  const id = parseInt(req.params.id, 10) as TodoId;
+  await adapter.delete({ id, done: false, label: "" }); // HACK fake Todo instance
+  res.end();
+});
+
+app.listen(3001, () => {
+  console.log("todoExamplePersistedRemoteStateServer listening on port 3001");
+});
