5b0473567c8a79743acb8a78be5db68d89b34dca
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-11 20:07:28 +0100

    Add path above app bar

--- src/apps/cinema/AppBar.tsx
+++ src/apps/cinema/AppBar.tsx
@@ -2,22 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "./Link";
 import { AppBar as MuiAppBar, Toolbar as MuiToolbar } from "@material-ui/core";
+import { useRoute } from "react-router5";
 
-export const AppBar = () => (
-  <MuiAppBar position="relative">
-    <Toolbar>
-      <Link color="inherit" routeName="home">
-        Home
-      </Link>
-      <Link color="inherit" routeName="tickets">
-        Tickets
-      </Link>
-      <Link color="inherit" routeName="movies">
-        Movies
-      </Link>
-    </Toolbar>
-  </MuiAppBar>
-);
+export const AppBar = () => {
+  const { route } = useRoute();
+  return (
+    <>
+      {route && route.path}
+      <MuiAppBar position="relative">
+        <Toolbar>
+          <Link color="inherit" routeName="home">
+            Home
+          </Link>
+          <Link color="inherit" routeName="tickets">
+            Tickets
+          </Link>
+          <Link color="inherit" routeName="movies">
+            Movies
+          </Link>
+        </Toolbar>
+      </MuiAppBar>
+    </>
+  );
+};
 
 const Toolbar = styled(MuiToolbar)`
   & {
