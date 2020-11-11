e50567b8d607d22ac2d1b6403820d7660b10e73b
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-11 20:05:54 +0100

    Remove excessive abstraction in useRouterMount

--- src/apps/cinema/CinemaApp.tsx
+++ src/apps/cinema/CinemaApp.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RouterProvider } from "react-router5";
 import { createRouterForConfig } from "../../lib/react-router5-extensions/createRouterForConfig";
-import { useRouterMount } from "../../lib/react-router5-extensions/useRouterMount";
 import { cinemaRoutes } from "./cinemaRoutes";
 import {
   CinemaRouteConfigContext,
@@ -11,7 +10,10 @@ import { AppBar } from "./AppBar";
 
 export const CinemaApp = () => {
   const [cinemaRouter] = useState(() => createRouterForConfig(cinemaRoutes));
-  useRouterMount(cinemaRouter);
+  useEffect(() => {
+    cinemaRouter.start();
+    return () => cinemaRouter.stop();
+  }, [cinemaRouter]);
   return (
     <RouterProvider router={cinemaRouter}>
       <CinemaRouteConfigContext.Provider value={cinemaRoutes}>
