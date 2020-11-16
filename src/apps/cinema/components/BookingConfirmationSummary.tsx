61ca6e1b1738c4923aee5baada97f979826a3200
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-16 20:59:51 +0100

    Stub components for the ticket confirmation page

---
+++ src/apps/cinema/components/BookingConfirmationSummary.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  Chip,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import { Card } from "./Card";
+import { Link } from "./Link";
+
+export const BookingConfirmationSummary = () => (
+  <>
+    <Row>
+      <Card backgroundSrc="http://lorempixel.com/180/280/transport/?_=1" />
+      <AlignBottom>
+        <Chip label="Premiär 13 november" color="primary" />
+        <Typography variant="h6">
+          <Link routeName="movie">Operation Nordpolen</Link>
+        </Typography>
+        <Typography>Tomorrow, tuesday 20:15</Typography>
+        <Typography>Filmstaden Heron City</Typography>
+        <Typography>Lounge 6</Typography>
+        <Typography variant="caption">1 hour 28 minutes</Typography>
+      </AlignBottom>
+    </Row>
+    <List>
+      <ListItem>
+        <ListItemText>2x Regular</ListItemText>
+        <ListItemSecondaryAction>
+          <Typography>270 kr</Typography>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  </>
+);
+
+const Row = styled.div`
+  display: flex;
+`;
+
+const AlignBottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-left: 16px;
+`;
--- src/apps/cinema/pages/BookingConfirmationPage.tsx
+++ src/apps/cinema/pages/BookingConfirmationPage.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 import { Link } from "../components/Link";
 import { Container } from "../components/Container";
+import { Button, TextField, Typography } from "@material-ui/core";
+import { PageActions } from "../components/PageActions";
+import { BookingConfirmationSummary } from "../components/BookingConfirmationSummary";
+import { useSnackbarValidator } from "../hooks/useSnackbarValidator";
+
+const emailRegex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/;
 
 export const BookingConfirmationPage = () => {
+  const [email, setEmail] = useState("");
+  const handleEmailChange = (
+    e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => setEmail(e.target.value);
+  const { snackbar, validate } = useSnackbarValidator(() => {
+    if (!email) {
+      return "You have to enter an e-mail address";
+    }
+    if (!emailRegex.test(email)) {
+      return "You have to enter a valid e-mail address";
+    }
+  });
   return (
-    <Container>
-      <div>BookingConfirmation</div>
-      <Link routeName="booking-success">
-        finish payment and get redirected back to app success page
-      </Link>
-      <br />
-      <Link routeName="booking-failure">
-        fail payment and get redirected back to app rejection page
-      </Link>
+    <Container maxWidth="sm">
+      <Typography variant="h4" paragraph>
+        Booking summary
+      </Typography>
+      <BookingConfirmationSummary />
+      <TextField label="E-mail" value={email} onChange={handleEmailChange} />
+      <PageActions>
+        <Link routeName="booking-seat-selection">
+          <Button variant="outlined">Return to seat selection</Button>
+        </Link>
+        <Link routeName="booking-success">
+          <Button variant="contained" color="primary" onClick={validate}>
+            Simulate payment success
+          </Button>
+        </Link>
+        <Link routeName="booking-failure">
+          <Button variant="contained" color="secondary" onClick={validate}>
+            Simulate payment failure
+          </Button>
+        </Link>
+      </PageActions>
+      {snackbar}
     </Container>
   );
 };
