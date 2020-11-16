82343143cdbe4c9c092f88cbd76c6dcb34f41d9d
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-16 19:34:04 +0100

    Stub components for the seat selection page

--- src/apps/cinema/components/CenterHorizontally.tsx
+++ src/apps/cinema/components/CenterHorizontally.tsx
@@ -2,5 +2,6 @@ import styled from "styled-components";
 
 export const CenterHorizontally = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 `;
--- src/apps/cinema/pages/BookingSeatSelectionPage.tsx
+++ src/apps/cinema/pages/BookingSeatSelectionPage.tsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { Autocomplete } from "@material-ui/lab";
 import { Link } from "../components/Link";
 import { Container } from "../components/Container";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import { CenterHorizontally } from "../components/CenterHorizontally";
 
 export const BookingSeatSelectionPage = () => {
+  const [seats, setSeats] = useState<string[]>([]);
   return (
     <Container>
-      <div>BookingSeatSelection</div>
-      <Link routeName="booking-ticket-selection">
-        return to ticket selection
-      </Link>
+      <Autocomplete
+        options={[
+          "Seat 1",
+          "Seat 2 - Extra leg room",
+          "Seat 3",
+          "Seat 4",
+          "Seat 5",
+          "Seat 6",
+        ]}
+        renderInput={(params) => (
+          <TextField {...params} label="Seats" variant="outlined" />
+        )}
+        value={seats}
+        onChange={(e, newValues) => setSeats(newValues)}
+        multiple
+      />
       <br />
-      <Link routeName="booking-confirmation">proceed to confirmation</Link>
+      <CenterHorizontally>
+        <Link routeName="booking-ticket-selection">
+          <Button variant="outlined">Return to ticket selection</Button>
+        </Link>
+        <Link routeName="booking-confirmation">
+          <Button variant="contained" color="primary">
+            proceed to confirmation
+          </Button>
+        </Link>
+      </CenterHorizontally>
     </Container>
   );
 };
