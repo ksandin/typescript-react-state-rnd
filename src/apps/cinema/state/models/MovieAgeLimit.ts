a576f98b0b84cd1cba7ed34b1634f440093aa817
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-19 01:18:48 +0100

    Children -> For children

--- src/apps/cinema/state/models/MovieAgeLimit.ts
+++ src/apps/cinema/state/models/MovieAgeLimit.ts
@@ -1,7 +1,7 @@
 export enum MovieAgeLimit {
-  All = "All",
-  Children = "Children",
-  Year7 = "7 years",
-  Year11 = "11 years",
-  Year15 = "15 years",
+  All = "All ages",
+  Children = "For children",
+  Year7 = "7 years+",
+  Year11 = "11 years+",
+  Year15 = "15 years+",
 }
