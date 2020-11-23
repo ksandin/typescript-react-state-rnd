71113b3bb762b33bc71388c12ed2cf8620d21a97
Kasper Sandin <kasper.sandin@gmail.com>
2020-11-23 19:52:37 +0100

    Make grid columns an attribute in ControlRow.tsx

--- src/apps/cinema/components/ControlRow.tsx
+++ src/apps/cinema/components/ControlRow.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
-export const ControlRow = styled.div`
+export const ControlRow = styled.div<{ columns?: number }>`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(${({ columns = 3 }) => columns}, 1fr);
   grid-template-rows: 1fr;
   grid-column-gap: 8px;
   grid-row-gap: 8px;
